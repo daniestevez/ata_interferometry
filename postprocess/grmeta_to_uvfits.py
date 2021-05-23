af40fb953b735d6ed243fdb24eea19c9090e0897
Daniel Estévez <daniel@destevez.net>
2021-05-23 19:53:31 +0200

    grmeta_to_uvfits.py: allow processing files by chunks
    This is used to process very large files that do not fit in RAM.
    The file is processed chunk-by-chunk and each chunk ends up as
    a different UVFITS file.

--- postprocess/grmeta_to_uvfits.py
+++ postprocess/grmeta_to_uvfits.py
@@ -9,7 +9,7 @@ import argparse
 import json
 import pathlib
 
-from astropy.time import Time
+from astropy.time import Time, TimeDelta
 from gnuradio.blocks import parse_file_metadata
 import pmt
 import numpy as np
@@ -30,34 +30,51 @@ def read_meta(path):
     return header
 
 
-def load_correlations(r, path, nfft):
+def total_timestamps(r, path, nfft):
     size = (path / (r + f'power_0')).stat().st_size
-    N = size // (4 * nfft)  # number of timestamps (4 is sizeof(float32))
+    return size // (4 * nfft)  # 4 is sizeof(float32)
+
+
+def load_correlations(r, path, nfft, first_timestamp=0, n_timestamps=None):
+    N = (n_timestamps if n_timestamps is not None
+         else total_timestamps(r, path, nfft))
+    offset_c64 = first_timestamp * nfft * 8
+    offset_f32 = first_timestamp * nfft * 4
     # Order is (time, frequency, baseline, polarization)
     x = np.zeros((N, nfft, 3, 4), dtype='complex64')
+    min_size = N
     for j in range(4):
         baseline = 2 if j >= 2 else 0
         polarization = 0 if j % 2 == 0 else 3
-        a = np.fromfile(path / (r + f'power_{j}'), dtype='float32')
-        a = a[:N*nfft].reshape((-1, nfft))
+        a = np.fromfile(path / (r + f'power_{j}'), dtype='float32',
+                        offset=offset_f32, count=N*nfft)
+        a = a[:a.size//nfft*nfft]
+        a = a.reshape((-1, nfft))
         x[:a.shape[0], :, baseline, polarization] = a
+        min_size = min(a.shape[0], min_size)
     for j in range(1, 5):
         # Swap polarizations XY and YX because we are swapping the baseline
         # order below also.
         polarization = [0, 2, 1, 3][j - 1]
-        a = np.fromfile(path / (r + f'cross_{j}'), dtype='complex64')
-        a = a[:N*nfft].reshape((-1, nfft))
+        a = np.fromfile(path / (r + f'cross_{j}'), dtype='complex64',
+                        offset=offset_c64, count=N*nfft)
+        a = a[:a.size//nfft*nfft]
+        a = a.reshape((-1, nfft))
         # Complex conjugate here is because the baseline in the correlation
         # file is 0-1 rather than 1-0 (which is required for lower triangular
         # format)
         x[:a.shape[0], :, 1, polarization] = np.conjugate(a)
+        min_size = min(a.shape[0], min_size)
     for j in (0, 5):
         baseline = 0 if j == 0 else 2
-        a = np.fromfile(path / (r + f'cross_{j}'), dtype='complex64')
-        a = a[:N*nfft].reshape((-1, nfft))
+        a = np.fromfile(path / (r + f'cross_{j}'), dtype='complex64',
+                        offset=offset_c64, count=N*nfft)
+        a = a[:a.size//nfft*nfft]
+        a = a.reshape((-1, nfft))
         x[:a.shape[0], :, baseline, 1] = a  # XY
         x[:a.shape[0], :, baseline, 2] = np.conjugate(a)  # YX
-    return x
+        min_size = min(a.shape[0], min_size)
+    return x[:min_size]
 
 
 def parse_args():
@@ -77,6 +94,8 @@ def parse_args():
     parser.add_argument(
         'f_avg', metavar='F_AVG', type=int, help='frequency average')
     parser.add_argument('output', metavar='OUTPUT', help='output directory')
+    parser.add_argument('--max_timestamps', type=int,
+                        help='maximum number of timestamps in RAM')
     return parser.parse_args()
 
 
@@ -126,24 +145,35 @@ def main():
         ants = [baseline.split('-')[0], baseline.split('-')[1]]
         baselines = ['-'.join([ants[0], ants[0]]), baseline,
                      '-'.join([ants[1], ants[1]])]
-        x = load_correlations(scan, input_path, nfft)
 
-        # Hack to swap polarizations on 2b and 1c
-        x = x[..., pol_swap(baseline)]
+        timestamps = total_timestamps(scan, input_path, nfft)
+        offsets = ([0] if not hasattr(args, 'max_timestamps')
+                   else range(0, timestamps, args.max_timestamps))
+        if len(offsets) > 1:
+            print(f'File is too large, processing in {len(offsets)} chunks')
+        for offset in offsets:
+            x = load_correlations(
+                scan, input_path, nfft, offset,
+                timestamps if len(offsets) == 1 else args.max_timestamps)
 
-        if x.size == 0:
-            print('Skipping empty file')
-            continue
+            # Hack to swap polarizations on 2b and 1c
+            x = x[..., pol_swap(baseline)]
 
-        uvw = phasing.apply_phasing(
-            phasing.midpoint_timestamps(t0, x.shape[0], T),
-            x, source, freq, samp_rate / nfft,
-            utils.filter_by_antennas(ants, ant_coordinates),
-            utils.filter_by_antennas(ants, delays) * 1e-9)
+            if x.size == 0:
+                print('Skipping empty file')
+                break
 
-        uvfits.export_uvfits(
-            t0, T, x, uvw, source_name, source, freq, samp_rate / nfft,
-            baselines, ant_coordinates, args.t_avg, args.f_avg, output_path)
+            t = t0 + TimeDelta(T * offset, format='sec')
+            uvw = phasing.apply_phasing(
+                phasing.midpoint_timestamps(t, x.shape[0], T),
+                x, source, freq, samp_rate / nfft,
+                utils.filter_by_antennas(ants, ant_coordinates),
+                utils.filter_by_antennas(ants, delays) * 1e-9)
+
+            uvfits.export_uvfits(
+                t, T, x, uvw, source_name, source, freq, samp_rate / nfft,
+                baselines, ant_coordinates, args.t_avg, args.f_avg,
+                output_path)
 
 
 if __name__ == '__main__':
